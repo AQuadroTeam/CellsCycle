8ff501de305c3817fd0605f4dbb1d94d76901b03
Ablablab <andrea.gennusa@gmail.com>
2016-08-16 01:35:49 +0200

    added initial version of MemoryThread

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 from CellCycle.Settings.SettingsManager import SettingsManager
 from CellCycle.Logger.Logger import LoggerHelper
-
+from CellCycle.MemoryThread.MemoryThread import startMemoryThread
+from threading import Thread
 
 SETTINGSFILEPATH = "./config.txt"
 
@@ -10,3 +11,6 @@ settings = SettingsManager().readConfigurationFromFile(SETTINGSFILEPATH)
 
 # setup logger. to write messages: logger.warning("hello warning"), logger.exception(""), logger.debug("Hi,I'm a bug")
 logger = LoggerHelper(settings).logger
+
+# start memory thread
+Thread(name='MemoryThread',target=startMemoryThread, args=[settings, logger]).start()
