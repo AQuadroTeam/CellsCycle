b993ea0df0710a9e112276801cc1a6b1d0193d27
Ablablab <andrea.gennusa@gmail.com>
2016-08-16 01:19:35 +0200

    added loggerHelper

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 from CellCycle.SettingsReader.SettingsManager import SettingsManager
+from CellCycle.Logger.Logger import LoggerHelper
 
 SETTINGSFILEPATH = "./config.txt"
 
 # read settings from config.txt
 settings = SettingsManager().readConfigurationFromFile(SETTINGSFILEPATH)
+logger = LoggerHelper(settings)
