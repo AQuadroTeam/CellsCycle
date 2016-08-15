eecad00747a8e099481a04e11b5a1c0afa5881f1
Ablablab <andrea.gennusa@gmail.com>
2016-08-16 01:24:15 +0200

    added comments and examples to loggerHelper

--- start.py
+++ start.py
@@ -6,4 +6,6 @@ SETTINGSFILEPATH = "./config.txt"
 
 # read settings from config.txt
 settings = SettingsManager().readConfigurationFromFile(SETTINGSFILEPATH)
-logger = LoggerHelper(settings)
+
+# setup logger. to write messages: logger.warning("hello warning"), logger.exception(""), logger.debug("Hi,I'm a bug")
+logger = LoggerHelper(settings).logger
