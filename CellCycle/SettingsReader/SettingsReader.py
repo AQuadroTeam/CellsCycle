379eb7f16b7d4501fd74556d1a9c030d4447bca5
alessandro.fazio <agil3b3ast@hotmail.it>
2016-08-19 18:14:58 +0200

    beginning commit

--- CellCycle/SettingsReader/SettingsReader.py
+++ CellCycle/SettingsReader/SettingsReader.py
@@ -1,19 +1,18 @@
 #! /usr/bin/env python
 FILE_PATH = "./config.txt"
 
+
 class SettingsReader:
-
     def __init__(self):
-         self.configDict = {}
-         self.filePath = FILE_PATH
-
+        self.configDict = {}
+        self.filePath = FILE_PATH
 
     def readConfigurationFile(self):
-         with open(FILE_PATH, 'r') as f:
-             for line in f:
-                 splitLine = line.split()
-                 self.configDict[splitLine[0]] = splitLine[1:]
+        with open(FILE_PATH, 'r') as f:
+            for line in f:
+                splitLine = line.split()
+                self.configDict[splitLine[0]] = splitLine[1:]
 
     def printAllSettings(self):
-        for (key,value) in self.configDict.iteritems() :
-            print key,value
+        for (key, value) in self.configDict.iteritems():
+            print("%s , %s" % key, value)
