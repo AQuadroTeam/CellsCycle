0e148fdd5565ea1254df19ac3530f1c0f8d79282
Ablablab <andrea.gennusa@gmail.com>
2016-08-21 19:41:27 +0200

    updated lru cache, added get slab, get ,set

--- CellCycle/MemoryThread/Cache.py
+++ CellCycle/MemoryThread/Cache.py
@@ -1,18 +1,19 @@
-import array
-
+from array import array as C_Array
+from sys import getsizeof
+from LynkedList import LynkedList
 class CacheSlubLRU:
-    
+
     def __init__(self, totalSize, slabSize):
 
         self.slabSize = slabSize
         self.totalSize = totalSize
 
 
-        self.slabArray = array.array('c','0'*self.totalSize) #empty char array
+        self.slabArray = C_Array('c','0'*self.totalSize) #empty char array
 
-        self.unused = []
-        self.partial = []
-        self.complete = []
+        self.unused = LynkedList()
+        self.partial = LynkedList()
+        self.complete = LynkedList()
 
         self.lru = [] #list of slabs, ordered by last use
 
@@ -21,15 +22,69 @@ class CacheSlubLRU:
         self.slabNumber = int(self.totalSize / self.slabSize)
 
         for slabIndex in range(self.slabNumber):
-            slab = Slab(slabIndex,self.slabSize, self.slabNumber, self.totalSize)
+            slab = Slab(self.slabArray, slabIndex,self.slabSize, self.slabNumber, self.totalSize)
             self.lru.append(slab)
             self.unused.append(slab)
 
-        print "done"
+    def getSlab(self, size):
+        for slab in self.partial:
+            if slab.availableSpace >= size:
+                return slab
+        # no partial compatible slabs
+        if self.unused.isEmpty():
+            #activate lru purge
+            slab = self.lru[len(self.lru)-1]
+            if (slab.state == 1):#partial
+                self.partial.pop(slab)
+                slab.clearSlab()
+                self.unused.push(slab)
+                return slab
+            if (slab.state == 2):#complete
+                self.complete.pop(slab)
+            if (slab.state == 0):#unused
+                self.unused.pop(slab)
+            self.updateLRUn(slab,3)
+
+        else:
+            #return an unused slab
+            slab = self.unused.pop()
+            self.updateLRUn(slab,3)
+            return slab
+
+    def set(self, key, value):
+        valueSize = getsizeof(value)
+
+        slab = self.cache.get(key)
+
+        if slab== None:
+            slab = self.getSlab(valueSize)
+            slab.addValue(key, value)
+            self.updateLru(slab)
+        else:
+            if less size:
+                slab.value[key] = value
+            else:
+                cache[key] = None
+
+    def get(self, key):
+        
+
+    def updateLRU(self, slab):
+        index = self.lru.index(slab)
+        if index > 0 :
+            nextOne = self.lru[index-1]
+            self.lru[index] = nextOne
+            self.lru[index-1] = slab
+
+    def updateLRUn(self, slab, n):
+        for i in range(n):
+            self.updateLRU(slab)
+
 class Slab:
 
-    def __init__(self, slabIndex,slabSize, slabNumber, totalSize):
-
+    def __init__(self, slabArray, slabIndex,slabSize, slabNumber, totalSize):
+        self.slabSize = slabSize
+        self.slabArray = slabArray
         self.availableSpace = int(slabSize)  #in bytes
         self.state = 0 #0 unused, 1 partial, 2 complete
         self.value = {} #elements like ("1234", "4", "10") that means, value of key 1234 begins at 4 and ends at 10
@@ -37,5 +92,7 @@ class Slab:
         self.begin = int( slabIndex * slabSize )
         self.end = int( (slabIndex+1) * slabSize ) -1
 
-if __name__ =="__main__":
-    cache = CacheSlubLRU(12000000000 , 1000000)
+    def clearSlab(self):
+        self.availableSpace = self.slabSize
+        self.value.clear()
+        self.state = 0
--- CellCycle/MemoryThread/MemoryThread.py
+++ CellCycle/MemoryThread/MemoryThread.py
@@ -1,4 +1,17 @@
+from Cache import Slab, CacheSlubLRU
+
 def startMemoryThread(settings, logger):
 
     logger.debug("Hello, I'm a funny thread")
     logger.debug(settings)
+
+    logger.debug("Initializing object pool...")
+    cache = CacheSlubLRU(10000000 , 1000000) #set as 10 mega, 1 mega per slab
+    logger.debug("Initializing object pool... done")
+
+    import sys,time
+    time.sleep(30)
+    print "slabArray " +str(sys.getsizeof(cache.slabArray))
+    print "unused " + str(sys.getsizeof(cache.unused))
+    print "lru " + str(sys.getsizeof(cache.lru))
+    print "singleslab" + str(sys.getsizeof(cache.lru[1]))
