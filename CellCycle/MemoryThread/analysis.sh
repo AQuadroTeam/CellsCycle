f532f637a5d76f0c9e3afb6cf046ccea99e7c913
Ablablab <andrea.gennusa@gmail.com>
2016-08-26 16:06:56 +0200

    updated

--- CellCycle/MemoryThread/analysis.sh
+++ CellCycle/MemoryThread/analysis.sh
@@ -20,6 +20,10 @@ python -m cProfile Cache.py 10000000 100000 100000 300 1 >> recst.txt
 python -m cProfile Cache.py 10000000 100000 500000 300 1 >> recst.txt
 python -m cProfile Cache.py 10000000 100000 1000000 300 1 >> recst.txt
 python -m cProfile Cache.py 10000000 100000 5000000 300 1 >> recst.txt
+python -m cProfile Cache.py 1000000000 1000000 100000 300 5 >> recst.txt
+python -m cProfile Cache.py 1000000000 1000000 500000 300 5 >> recst.txt
+python -m cProfile Cache.py 1000000000 1000000 1000000 300 5 >> recst.txt
+python -m cProfile Cache.py 1000000000 1000000 5000000 300 5 >> recst.txt
 echo ll array
 echo ll array UPDATE >> reclla.txt
 git checkout CacheLinkedListArrays
@@ -42,4 +46,8 @@ python -m cProfile Cache.py 10000000 100000 100000 300 1 >> reclla.txt
 python -m cProfile Cache.py 10000000 100000 500000 300 1 >> reclla.txt
 python -m cProfile Cache.py 10000000 100000 1000000 300 1 >> reclla.txt
 python -m cProfile Cache.py 10000000 100000 5000000 300 1 >> reclla.txt
+python -m cProfile Cache.py 1000000000 1000000 100000 300 5 >> reclla.txt
+python -m cProfile Cache.py 1000000000 1000000 500000 300 5 >> reclla.txt
+python -m cProfile Cache.py 1000000000 1000000 1000000 300 5 >> reclla.txt
+python -m cProfile Cache.py 1000000000 1000000 5000000 300 5 >> reclla.txt
 poweroff
