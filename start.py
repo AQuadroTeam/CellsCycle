89d58f578ff0d3ef7212e92bd828b3df219d2c1f
Ablablab <andrea.gennusa@gmail.com>
2016-10-03 00:45:21 +0200

    fixed network implementation for set and get. example of usage in start.py

--- start.py
+++ start.py
@@ -16,25 +16,39 @@ logger = LoggerHelper(settings).logger
 url_worker, url_set, url_setPort, url_getPort = startMemoryTask(settings, logger, True)
 url_worker_slave, url_set_slave, url_setPort_slave, url_getPort_slave = startMemoryTask(settings, logger, False)
 
-#usage example
-import time
-time.sleep(5)
-url_getPort = "tcp://localhost:" + str(settings.getMasterGetPort())
-url_setPort = "tcp://localhost:" + str(settings.getMasterSetPort())
-url_setPort_slave = "tcp://localhost:" + str(settings.getSlaveSetPort())
-url_getPort_slave = "tcp://localhost:" + str(settings.getSlaveGetPort())
-getRequest(url_getPort, "12")
-setRequest(url_setPort, "12", "333asd")
 
-setRequest(url_setPort, "1", "asadasdasdsd")
-import time
-time.sleep(1)
-print "dovrebbe uscire asad....: " + str(getRequest(url_getPort, 1))
-print "non dovrebbe uscire asdasda...: " + str(getRequest(url_getPort_slave, 1))
+def exampleFillAndTransfer(settings, logger):
+    #usage example
+    import time
+    time.sleep(5)
+    url_getPort = "tcp://localhost:" + str(settings.getMasterGetPort())
+    url_setPort = "tcp://localhost:" + str(settings.getMasterSetPort())
+    url_setPort_slave = "tcp://localhost:" + str(settings.getSlaveSetPort())
+    url_getPort_slave = "tcp://localhost:" + str(settings.getSlaveGetPort())
+    getRequest(url_getPort, "12")
+    setRequest(url_setPort, "12", "333asd")
 
-transferRequest(url_setPort, url_setPort_slave)
-time.sleep(1)
-print "sul task principale ho ricevuto con chiave 1 su slave:" + str(getRequest(url_getPort_slave, 1))
-print "sul task principale ho ricevuto con chiave 1 su master: " + str(getRequest(url_getPort, 1))
-killProcess(url_setPort)
-killProcess(url_setPort_slave)
+    setRequest(url_setPort, "1", "asadasdasdsd")
+    import time
+    time.sleep(1)
+    import zmq
+    from cPickle import dumps, loads
+    from CellCycle.MemoryModule.MemoryManagement import Command, SETCOMMAND
+    context = zmq.Context.instance()
+    socket = context.socket(zmq.PUSH)
+    socket.connect(url_setPort)
+
+    for a in range(1000000):
+
+        socket.send(dumps(Command(SETCOMMAND, str(a), "aaaaaaaaa", address="url_setPort_slave")))
+    socket.close()
+
+    print "dovrebbe uscire asad....: " + str(getRequest(url_getPort, 1))
+    print "non dovrebbe uscire asdasda...: " + str(getRequest(url_getPort_slave, 1))
+
+    transferRequest(url_setPort, url_setPort_slave)
+    time.sleep(10)
+    print "sul task principale ho ricevuto con chiave 1 su slave:" + str(getRequest(url_getPort_slave, 1))
+    print "sul task principale ho ricevuto con chiave 1 su master: " + str(getRequest(url_getPort, 1))
+    killProcess(url_setPort)
+    killProcess(url_setPort_slave)
