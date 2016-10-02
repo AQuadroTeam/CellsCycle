ca3128bd1574db293944cf2b2425c8deade98809
Ablablab <andrea.gennusa@gmail.com>
2016-10-02 22:04:43 +0200

    updated to use network socket

--- start.py
+++ start.py
@@ -13,19 +13,26 @@ settings = SettingsManager().readConfigurationFromFile(SETTINGSFILEPATH)
 logger = LoggerHelper(settings).logger
 
 # start memory task. there's a thread for set/control requests, and n threads for get. getterNumber is a setting
-setMasterPipe, getMasterPipeList = startMemoryTask(settings, logger)
-setSlavePipe, getSlavePipeList = startMemoryTask(settings, logger)
+url_worker, url_set, url_setPort, url_getPort = startMemoryTask(settings, logger, True)
+url_worker_slave, url_set_slave, url_setPort_slave, url_getPort_slave = startMemoryTask(settings, logger, False)
 
 #usage example
-setRequest(setMasterPipe, "12", "333asd")
-setRequest(setMasterPipe, "1", "asadasdasdsd")
+import time
+time.sleep(5)
+url_getPort = "tcp://localhost:" + str(settings.getMasterGetPort())
+print url_getPort
+getRequest(url_getPort, "12")
+print "primo get"
+setRequest(url_setPort, "12", "333asd")
+
+setRequest(url_setPort, "1", "asadasdasdsd")
 import time
 time.sleep(1)
-print getRequest(getSlavePipeList[0], 1)
-print getRequest(getMasterPipeList[0], 1)
-cache = transferRequest(setMasterPipe)
-print cache.debug()
-print "sul task principale ho ricevuto con chiave 1 su slave:" + str(getRequest(getSlavePipeList[0], 1))
-print "sul task principale ho ricevuto con chiave 1 su master: " + str(getRequest(getMasterPipeList[0], 1))
-killProcess(setMasterPipe)
-killProcess(setSlavePipe)
+print getRequest(url_getPort, 1)
+print getRequest(url_getPort_slave, 1)
+cache = transferRequest(url_setPort)
+print len(cache)
+print "sul task principale ho ricevuto con chiave 1 su slave:" + str(getRequest(url_getPort_slave, 1))
+print "sul task principale ho ricevuto con chiave 1 su master: " + str(getRequest(url_getPort, 1))
+killProcess(url_setPort)
+killProcess(url_setPort_slave)
