31c90c8e74530dae9a0244abc431c63915500738
Ablablab <andrea.gennusa@gmail.com>
2016-11-04 20:11:11 +0100

    added stop and terminate api for aws

--- CellCycle/AWS/AWSlib.py
+++ CellCycle/AWS/AWSlib.py
@@ -7,3 +7,13 @@ def startInstanceAWS(settings, logger):
     imageIdCellCycle = settings.getAwsImageId()
     logger.debug("id image: " + imageIdCellCycle)
     ec2.create_instances(ImageId=imageIdCellCycle, MinCount=1, MaxCount=1, InstanceType='t2.micro')
+
+def terminateInstanceAWS(settings, logger, instanceID):
+    logger.debug("contact amazon to terminate : " + instanceID)
+    ec2 = boto3.resource('ec2')
+    ec2.instances.filter(InstanceIds=instanceID).terminate()
+
+def stopInstanceAWS(settings, logger, instanceID):
+    logger.debug("contact amazon to stop : " + instanceID)
+    ec2 = boto3.resource('ec2')
+    ec2.instances.filter(InstanceIds=instanceID).stop()
