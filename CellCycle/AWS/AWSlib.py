16253cebef5d9ffb58cc6de3fcc2880efcce3604
Ablablab <andrea.gennusa@gmail.com>
2016-11-04 20:12:59 +0100

    added stop and terminate this instance

--- CellCycle/AWS/AWSlib.py
+++ CellCycle/AWS/AWSlib.py
@@ -8,6 +8,18 @@ def startInstanceAWS(settings, logger):
     logger.debug("id image: " + imageIdCellCycle)
     ec2.create_instances(ImageId=imageIdCellCycle, MinCount=1, MaxCount=1, InstanceType='t2.micro')
 
+def terminateThisInstanceAWS(settings, logger):
+    import requests
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_id = response.text
+    terminateInstanceAWS(settings, logger, instance_id)
+
+def stopThisInstanceAWS(settings, logger):
+    import requests
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_id = response.text
+    stopInstanceAWS(settings, logger, instance_id)
+
 def terminateInstanceAWS(settings, logger, instanceID):
     logger.debug("contact amazon to terminate : " + instanceID)
     ec2 = boto3.resource('ec2')
