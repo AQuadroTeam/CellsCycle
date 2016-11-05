a9a64e5bd0085ab82fbbaa214adf18b136d04a75
alessandro.fazio <agil3b3ast@hotmail.it>
2016-11-05 18:35:20 +0100

    Changed Generator.py, now it accepts a json string as input parameter

--- CellCycle/ChainModule/Generator.py
+++ CellCycle/ChainModule/Generator.py
@@ -1,52 +1,54 @@
 #! /usr/bin/env python
 
-from multiprocessing import Process
-from argparse import ArgumentParser
 from ListThread import Node
 from DeadReader import DeadReader
 from DeadWriter import DeadWriter
+from json import loads
 
-ID_INDEX = 0
-IP_INDEX = 1
-MIN_KEY_INDEX = 2
-MAX_KEY_INDEX = 3
+MYSELF = 'myself'
+MASTER = 'master'
+SLAVE = 'slave'
+MASTER_OF_MASTER = 'master_of_master'
+SLAVE_OF_SLAVE = 'slave_of_slave'
 
+ID = 'id'
+IP = 'ip'
+MIN_KEY = 'min_key'
+MAX_KEY = 'max_key'
 
-def get_args():
-    parser = ArgumentParser(description='Process Cells Cycle')
-    parser.add_argument('myself', type=list, help='the node we are talking about')
-    parser.add_argument('master', type=list, help='the node\'s master')
-    parser.add_argument('slave', type=list, help='the node\'s slave')
-    parser.add_argument('master_of_master', type=list, help='the node\'s master of master')
-    parser.add_argument('slave_of_slave', type=list, help='the node\'s slave of slave')
+# unused
+# def get_args():
+#     parser = ArgumentParser(description='Process Cells Cycle')
+#     parser.add_argument('myself', type=list, help='the node we are talking about')
+#     parser.add_argument('master', type=list, help='the node\'s master')
+#     parser.add_argument('slave', type=list, help='the node\'s slave')
+#     parser.add_argument('master_of_master', type=list, help='the node\'s master of master')
+#     parser.add_argument('slave_of_slave', type=list, help='the node\'s slave of slave')
 
-    return parser.parse_args()
+#     return parser.parse_args()
 
 
 class Generator:
-    def __init__(self, logger, settings, args):
+    def __init__(self, logger, settings, json_arg):
         self.logger = logger
         self.settings = settings
-        self.args = args
+        self.args = loads(json_arg)
 
-    def _create_process_environment(self):
-        myself = self.args.myself
-        myself = Node(myself[ID_INDEX], myself[IP_INDEX], self.settings.getIntPort(),
-                      self.settings.getExtPort(), min_key=myself[MIN_KEY_INDEX], max_key=myself[MAX_KEY_INDEX])
-        master = self.args.master
-        master = Node(master[ID_INDEX], master[IP_INDEX], self.settings.getIntPort(),
-                      self.settings.getExtPort(), min_key=master[MIN_KEY_INDEX], max_key=master[MAX_KEY_INDEX])
-        slave = self.args.slave
-        slave = Node(slave[ID_INDEX], slave[IP_INDEX], self.settings.getIntPort(),
-                     self.settings.getExtPort(), min_key=slave[MIN_KEY_INDEX], max_key=slave[MAX_KEY_INDEX])
-        master_of_master = self.args.master_of_master
-        master_of_master = Node(master_of_master[ID_INDEX], master_of_master[IP_INDEX], self.settings.getIntPort(),
-                                self.settings.getExtPort(), min_key=master_of_master[MIN_KEY_INDEX],
-                                max_key=master_of_master[MAX_KEY_INDEX])
-        slave_of_slave = self.args.slave_of_slave
-        slave_of_slave = Node(slave_of_slave[ID_INDEX], slave_of_slave[IP_INDEX], self.settings.getIntPort(),
-                              self.settings.getExtPort(), min_key=slave_of_slave[MIN_KEY_INDEX],
-                              max_key=slave_of_slave[MAX_KEY_INDEX])
+    def get_node_from_data(self, data):
+        return Node(data[ID], data[IP], self.settings.getIntPort(),
+                    self.settings.getExtPort(), min_key=data[MIN_KEY], max_key=data[MAX_KEY])
+
+    def create_process_environment(self):
+        myself = self.args[MYSELF]
+        myself = self.get_node_from_data(myself)
+        master = self.args[MASTER]
+        master = self.get_node_from_data(master)
+        slave = self.args[SLAVE]
+        slave = self.get_node_from_data(slave)
+        master_of_master = self.args[MASTER_OF_MASTER]
+        master_of_master = self.get_node_from_data(master_of_master)
+        slave_of_slave = self.args[SLAVE_OF_SLAVE]
+        slave_of_slave = self.get_node_from_data(slave_of_slave)
 
         reader = DeadReader(myself, master, slave, slave_of_slave, master_of_master, self.logger, self.settings)
         writer = DeadWriter(myself, master, slave, slave_of_slave, master_of_master, self.logger, self.settings)
@@ -54,11 +56,12 @@ class Generator:
         reader.run()
         writer.run()
 
-    def create_process(self):
-        Process(name='ListCommunicationProcess', target=Generator._create_process_environment(self))
+    # unused
+    # def create_process(self):
+    #     Process(name='ListCommunicationProcess', target=Generator._create_process_environment(self))
 
 
-class Parameter:
+class Parameter:    # unused
 
     def __init__(self, myself, master, slave, master_of_master, slave_of_slave):
         self.myself = myself
@@ -66,3 +69,4 @@ class Parameter:
         self.slave_of_slave = slave_of_slave
         self.master = master
         self.master_of_master = master_of_master
+
