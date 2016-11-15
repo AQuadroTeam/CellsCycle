e7f03da2717eb35cd3e4524dce7307e77c1202f8
Ablablab <andrea.gennusa@gmail.com>
2016-11-15 19:33:09 +0100

    fixed get request to slave

--- CellCycle/ExtraCycleInterface/ExtraCycle.py
+++ CellCycle/ExtraCycleInterface/ExtraCycle.py
@@ -212,17 +212,16 @@ def _deleteHandler(settings, logger, socket,client, key, list_manager):
 def _getHandler(settings,logger,  socket, client, key, list_manager):
     from random import random
     #get server nodes and choose
-    host = list_manager.node_list.find_memory_key(key)
+    masterHost = list_manager.node_list.find_memory_key(key)
+    slaveHost = masterHost.slave
     if(random()>0.5):
         if(settings.isVerbose()):
-            logger.debug("sending get request to master " + str(host.target.ip))
-        returnValue =standardMasterGetRequest(settings, key, host.target.ip)
+            logger.debug("sending get request to master " + str(masterHost.ip))
+        returnValue =standardMasterGetRequest(settings, key, masterHost.ip)
     else:
         if(settings.isVerbose()):
-            logger.debug("sending get request to slave" + str(host.target.ip))
-        returnValue =standardSlaveGetRequest(settings, key, host.target.ip)
-    #
-    # TODO replace else ip with slave ip
+            logger.debug("sending get request to slave" + str(slaveHost.ip))
+        returnValue =standardSlaveGetRequest(settings, key, slaveHost.ip)
     #returnValue = standardMasterGetRequest(settings, key)
     returnValue = returnValue if returnValue!=None else ""
 
