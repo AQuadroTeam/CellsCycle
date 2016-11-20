d0b54529352393ba3ae382a00a0c27db25633047
Ablablab <andrea.gennusa@gmail.com>
2016-11-20 22:37:32 +0100

    added new image id, removed init script. it is no more necessary

--- launchMemaslap.py
+++ launchMemaslap.py
@@ -3,12 +3,11 @@ def launchMemaslap(settings):
   awsProfileName = settings.getAwsProfileName()
   ec2 = boto3.Session(profile_name=awsProfileName).resource('ec2')
 
-  imageIdCellCycle = "ami-4709cc28"
+  imageIdCellCycle = "ami-7e854111"
   keyName = settings.getAwsKeyName()
   securityGroup = settings.getAwsSecurityGroup()
   privateIp = "172.31.21.1"
-  userData = 'echo "alias memaslap-test="memaslap -s 172.31.20.1:5555,172.31.20.2:5555,172.31.20.3:5555,172.31.20.4:5555,172.31.20.5:5555"" >> /home/ubuntu/.bashrc\necho "\n\nUse memaslap-test to load system (on 172.31.20.1-5)\nUsage:\n\tmemaslap-test\n\tmemaslap-test -t 100s\n>> /home/ubuntu/.bashrc"'
-  ec2.create_instances(ImageId=imageIdCellCycle, MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName=keyName, SecurityGroups=[securityGroup],  PrivateIpAddress=privateIp, UserData = userData)
+  ec2.create_instances(ImageId=imageIdCellCycle, MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName=keyName, SecurityGroups=[securityGroup],  PrivateIpAddress=privateIp)
 
 if __name__ == "__main__":
     import sys
