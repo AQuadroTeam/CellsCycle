91089b7a0efa8e57c8280d91a77b2c5f4eeaa609
Alessandro <agil3b3ast@hotmail.it>
2016-12-03 14:56:58 +0100

    Added __str__ in MemoryObject

--- CellCycle/ChainModule/MemoryObject.py
+++ CellCycle/ChainModule/MemoryObject.py
@@ -10,6 +10,16 @@ class MemoryObject:
         self.slave = slave
         self.slave_of_slave = slave_of_slave
 
+    def __str__(self):
+        rel = [self.myself, self.master, self.master_of_master, self.slave, self.slave_of_slave]
+        to_print = ["myself", "master", "master_of_master", "slave", "slave_of_slave"]
+        str_to_print = ''
+        for x in xrange(len(rel)):
+            key = to_print[x]
+            val = rel[x]
+            str_to_print += 'Node {}, id {}, keys {}\n'.format(key, val.id, val.get_min_max_key())
+        return str_to_print
+
     def print_elements(self):
         rel = [self.myself, self.master, self.master_of_master, self.slave, self.slave_of_slave]
         to_print = ["myself", "master", "master_of_master", "slave", "slave_of_slave"]
